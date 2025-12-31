02c86bcf83608fce9c9d9adaad1318ec201a5987
yyd <157128184+Starlight99yyds@users.noreply.github.com>
2026-01-01 05:37:25 +0800

    Create config_loader.py

---
+++ sample/configs/config_loader.py
@@ -0,0 +1,23 @@
+import configargparse
+
+
+def get_config():
+    parser = configargparse.ArgumentParser()
+
+    # 基础参数
+    parser.add_argument('--batch_size', type=int, default=4)
+    parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+
+    # 数据参数
+    parser.add_argument('--data_dir', type=str, default='./data')
+    parser.add_argument('--model_save_dir', type=str, default='./checkpoints')
+
+    # 模型参数
+    parser.add_argument('--in_channels', type=int, default=1)
+    parser.add_argument('--out_channels', type=int, default=2)
+
+    # 训练控制
+    parser.add_argument('--save_freq', type=int, default=10)
+
+    return parser.parse_args()
